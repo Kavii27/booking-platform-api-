eec358283c0265d716d2115c95d69195bc27ded4
Kaveesha Sandeepani <kaveeshasandeepani027@gmail.com>
2026-07-11 13:27:23 +0530

    add global HTTP exception filter for improved error handling

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}));
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Booking Platform API')
